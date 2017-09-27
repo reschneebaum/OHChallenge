c4c8ec07cc43afc928f1b5969adef6b569f7f843
Rachel Schneebaum <reschneebaum@gmail.com>
2017-09-27 16:12:42 -0500

    Initial Commit

---
+++ OviaChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OviaChallenge
+//
+//  Created by Rachel Schneebaum on 9/27/17.
+//  Copyright © 2017 Rachel Schneebaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
